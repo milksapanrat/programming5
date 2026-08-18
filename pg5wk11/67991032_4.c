cb2734b52551baa87ca08918bf3cf8d697116e8d
milksapanrat <67991032@kmitl.ac.th>
2026-08-18 09:52:45 +0700

    wk11ex4

--- pg5wk11/67991032_4.c
+++ pg5wk11/67991032_4.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    printf("my name is milk\n");
+int main(void){
+    int rows, columns;
+
+    printf("How many rows do you want? ");
+    scanf("%d", &rows);
+    printf("How many columns do you want? ");
+    scanf("%d", &columns);
+
+    int **grid = malloc(rows * sizeof(int*));
+    for (int i = 0; i < rows; i++) {
+        grid[i] = malloc(columns * sizeof(int));
+    }
+
+    int number = 1;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            grid[i][j] = number++;
+        }
+    }
+
+    printf("--- My Magic Grid ---\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            printf("%d ", grid[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < rows; i++) {
+        free(grid[i]);
+    }
+    free(grid);
+
+    return 0;
 }
